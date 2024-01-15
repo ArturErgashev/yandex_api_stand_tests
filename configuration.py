e96860ebabd801b861e8477e4feb5e0a912c9bc0
AlphaOrionis <artur-bel@mail.ru>
2024-01-15 19:36:33 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://2cd6303b-7317-41d5-b51d-a090a726c312.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
